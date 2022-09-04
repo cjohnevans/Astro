eb0950d84df505df17d80a6956cb5958af28bfb5
cjohnevans <c.john.evans@gmail.com>
2022-09-04 19:55:42 +0100

    moved source data dir, tidy up

--- load_aavso_light_curve.py
+++ load_aavso_light_curve.py
@@ -4,16 +4,23 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
-from astropy.table import Table  
+from astropy.table import Table
+import os
 
-lightcurve_file = 'aavsodata_v1405.csv'
+data_dir = '/home/john/astro/variable_star_data'
+#lightcurve_file = 'aavsodata_v1405cas_20220903.csv'
+lightcurve_file = 'aavsodata_txdra_20220904.csv'
+lightcurve_path = os.path.join(data_dir, lightcurve_file)
+#chart_title = 'V1405 Cas'
+chart_title = 'TX Dra'
 
-data = Table.read(lightcurve_file, encoding='UTF-8')
+data = Table.read(lightcurve_path, encoding='UTF-8')
 print(data.colnames)
 
 #watch out - Magnitude gets interpreted as TEXT due to the 'fainter thans'
 rows_to_remove = []
 for observation in data:
+    #print(observation['Star Name'])
     if '<' in observation['Magnitude']:
         rows_to_remove.append(observation.index)
 # remove rows with text
@@ -25,7 +32,7 @@ data['Magnitude'] = [float(mag) for mag in data['Magnitude']]
 #        observation['Magnitude'] = float(observation['Magnitude'])
 #    print(type(observation['Magnitude']))
 
-plt.scatter(data['JD'], data['Magnitude'])
+plt.scatter(data['JD'], data['Magnitude'], marker='.')
 plt.gca().invert_yaxis()
-plt.title('V1405 Cas')
+plt.title(chart_title)
 plt.show()
