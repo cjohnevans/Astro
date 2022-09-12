e1138f95e26d76206817f75f23fc7caccde2f3cf
cjohnevans <c.john.evans@gmail.com>
2022-09-12 21:11:34 +0100

    julian date calc

--- julian.py
+++ julian.py
@@ -1,9 +1,46 @@
 import numpy as np
 from astropy.time import Time
 from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
-t_dt = datetime(2021,10,11)
-print(t_dt)
-t2 = Time(t_dt)
-print(t2)
-print(t2.jd)
+def get_year_jd(year_span):
+    year_val = []
+    jd_val = []
+    for yy in year_span:
+        print(yy)
+        t_dt = datetime(int(yy),1,1)
+        t_astro = Time(t_dt)
+        #print(t_astro.decimalyear, t_astro.jd)
+        year_val.append(t_astro.decimalyear)
+        jd_val.append(t_astro.jd)
+    return (year_val, jd_val)
+
+def plot_year_jd(year_val, jd_val, lo, hi, step):
+    fig, ax = plt.subplots()
+    plt.plot(year_val, jd_val)
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
+    plt.xticks(year_val)
+    plt.yticks(np.arange(lo, hi, step))
+    plt.grid(axis='both')
+    ax.tick_params(axis='x', labelrotation=90)
+    plt.ylabel('JD')
+    plt.xlabel('year')    
+    
+yr_span = np.arange(1900, 2040, 10.0)
+yr_span_short = np.arange(2000,2026,1.0)
+print(yr_span)
+print(yr_span_short)
+
+
+(year_val_1, jd_val_1) = get_year_jd(yr_span)
+print(year_val_1)
+plot_year_jd(year_val_1, jd_val_1, 2410000, 2465000, 5000)
+    
+(year_val_2, jd_val_2) = get_year_jd(yr_span_short)
+plot_year_jd(year_val_2, jd_val_2, 2451000, 2461000, 500)
+print(year_val_2)
+plt.show()
+
+
+    
--- load_aavso_light_curve.py
+++ load_aavso_light_curve.py
@@ -5,7 +5,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from astropy.table import Table
+from astropy.time import Time
+import lightkurve as lk
 import os
+
+def plot_lightcurve(data_JD, data_mag, JDmin, JDmax):
+    plt.scatter(data_JD, data_mag, marker='.')
+    plt.gca().invert_yaxis()
+    plt.title(chart_title)
+    plt.xlim([JDmin, JDmax])
+    xvals = plt.gca().get_xticks()
+    plt.gca().set_xticklabels(['{:,.0f}'.format(x) for x in xvals])
+    plt.show()    
 
 data_dir = '/home/john/astro/variable_star_data'
 #lightcurve_file = 'aavsodata_v1405cas_20220903.csv'
@@ -28,11 +39,13 @@ print(rows_to_remove)
 data.remove_rows(rows_to_remove)
 
 data['Magnitude'] = [float(mag) for mag in data['Magnitude']]
-#    else:
-#        observation['Magnitude'] = float(observation['Magnitude'])
-#    print(type(observation['Magnitude']))
 
-plt.scatter(data['JD'], data['Magnitude'], marker='.')
-plt.gca().invert_yaxis()
-plt.title(chart_title)
-plt.show()
+plot_lightcurve(data['JD'], data['Magnitude'], 2451000, 2460000)
+##
+##plt.scatter(data['JD'], data['Magnitude'], marker='.')
+##plt.gca().invert_yaxis()
+##xvals = plt.gca().get_xticks()
+##print(xvals)
+##plt.gca().set_xticklabels(['{:,.0f}'.format(x) for x in xvals])
+##plt.title(chart_title)
+##plt.show()
