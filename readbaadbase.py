9ecfb714ff418a6a7a653ee26aa7e14a38da1ec2
cjohnevans <c.john.evans@gmail.com>
2022-09-12 22:21:13 +0100

    tidy up

--- readbaadbase.py
+++ readbaadbase.py
@@ -4,24 +4,24 @@
 
 import csv
 import matplotlib.pyplot as plt
-from scipy import optimize
 import numpy as np
 
-vssdir='C:\\Users\\cjohn\\OneDrive\\Astronomy\\variablestars\\data_baa'
+vssdir='/home/john/astro/variable_star_data'
 #vssfile='Z UMA_20210619_100143.csv'
 #vssfile='AH DRA_20210621_221943.csv'
 #vssfile='AH DRA_test.csv'
-vssfile = 'Z UMA_20210626_235405.csv'
+#vssfile = 'Z UMA_20210626_235405.csv'
+vssfile = 'Z UMA_20220912_201610.csv'
 starname=vssfile.split('_')[0]
 print(starname)
-vssfullpath = vssdir + '\\' + vssfile
+vssfullpath = vssdir + '/' + vssfile
 
 jd = []
 mag = []
 
 #using the (large) JD as x axis makes fitting unstable in period?
 # use days relative to first measurement instead
-jdmodifier = 2400000.0  #discard first N digits of jd here
+jdmodifier = 00000.0  #discard first N digits of jd here
 
 with open(vssfullpath) as csvfile:
     baaread = csv.DictReader(csvfile, delimiter=',')
@@ -38,28 +38,9 @@ fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 ax1.set_ylabel('Mag.')
 ax1.set_xlabel('time (days)')
-ax1.plot(time_day, mag, '.')
+ax1.plot(jd, mag, '.')
 ax1.set_title(starname)
-fig.show()
 
-def sin_fit(x, amplitude, period, offset, phase):
-    return offset + amplitude * np.sin(  2* np.pi * (x + phase) / period)
-
-ub = (np.inf, np.inf, np.inf, 100)
-lb = (0, 0, 0, 0)
-
-#ub = (np.inf, np.inf, np.inf, 2*np.pi)
-#lb = (-np.inf, -np.inf, -np.inf, 0)
-
-#params, params_cov = optimize.curve_fit(sin_fit, jd, mag, p0=[1, 20, 8, 0])
-params, params_cov = optimize.curve_fit(sin_fit, \
-                                        time_day, \
-                                        mag, \
-                                        p0=[2, 200, 8, 50], \
-                                        bounds=(lb,ub))
-print(params)
-
-ax1.plot(time_day, sin_fit(time_day, amplitude = params[0], period = params[1],
-                     offset = params[2], phase = params[3]))
-         
+plt.show()
+     
          
